cf08e4edb9a73e016189ffb1b42833c960713f0f
JEKSER1ST1 <74066008+JEKSER1ST1@users.noreply.github.com>
2022-01-26 20:55:30 +0200

    Add files via upload

---
+++ lesson29/dz.py
@@ -0,0 +1,13 @@
+class Resturant:
+    def __init__ (self,name,cuisine_type,age):
+        self.name=name
+        self.cuisine_type=cuisine_type
+        self.age=age
+    def describe_resturant(self):
+    print("Ресторан открыт ")
+
+    def open_resturant()
+    print("Ресторан открыт")
+
+my_resturant = Resturant("Den y Dena", 0 , 5 years)
+print("my_restutant.name")
